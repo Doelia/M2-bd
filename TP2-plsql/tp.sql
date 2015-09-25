8d53020f02f1ccb448ba922dbad8e2af38122622
doelia <kyxalia@gmail.com>
2015-09-25 15:29:29 +0200

    Ajout question 1

---
+++ TP2-plsql/tp.sql
@@ -0,0 +1,23 @@
+-- TP2
+
+-- 2.1
+-- Construisez un trigger qui v ́erifie que la population de 2010 (pop 2010 de Commune) est toujours
+-- positive (ordres insert et update)
+CREATE OR replace TRIGGER verif_pop
+before INSERT OR update on Commune
+for each row
+begin
+	if (:new.POP_2010 < 0) then
+		raise_application_error(-20500, 'La population doit être positive');
+	end if;
+end;
+/
+
+-- 2.2
+create or replace procedure
+	JoursEtHeuresOuvrables
+begin
+	
+
+end;
+/
