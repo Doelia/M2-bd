2e924637e81df9bcb64dd797f33be4e539e0ee61
Doelia <kyxalia@gmail.com>
2015-10-31 20:22:52 +0100

    Ajout requete de get shema pour un user

--- Projet/part1.sql
+++ Projet/part1.sql
@@ -1,7 +1,7 @@
 
 set long 40000;
 
--- Donne la requête de création de la table
+-- Donne la requête de création de la table commune
 select DBMS_METADATA.GET_DDL('TABLE','COMMUNE') from DUAL;
 
 
@@ -14,3 +14,12 @@ exec DBMS_METADATA.set_transform_param(DBMS_METADATA.session_transform, 'TABLESP
 spool communes.sql
 select DBMS_METADATA.GET_DDL('TABLE','COMMUNE', USER) from user_tables;
 spool off
+
+exec dbms_metadata.set_transform_param(dbms_metadata.session_transform,'SQLTERMINATOR', true);
+SELECT dbms_metadata.get_ddl('TABLE', TABLE_NAME, 'STEPHANE') FROM user_tables;
+
+-- Liste des tables pour un user
+SELECT TABLE_NAME FROM DBA_TABLES WHERE OWNER='STEPHANE';
+
+-- Schéma de construction pour toutes les tables d'un user
+SELECT dbms_metadata.get_ddl('TABLE', TABLE_NAME, 'STEPHANE') FROM DBA_TABLES WHERE OWNER='STEPHANE';
